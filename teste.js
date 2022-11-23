8b988efad4602a3445c8452363f2aa838e0df243
Arthur Trajano <trajaninho1@gmail.com>
2022-11-23 09:45:47 -0300

    Versão 1

--- teste.js
+++ teste.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 http.createServer(function(req, res){
     res.writeHead(200,{'Content-Type': 'text/html'})
+    res.write(Data());
     res.end("<center> <h2> Olá, Boa tarde!</h2></center>")
 }).listen(80);
     
